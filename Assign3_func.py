6ecddc73fe3e9ff3d19b0867a2e0270d238eaba6
Mary Joy Aban <92615862+MJA1704@users.noreply.github.com>
2021-11-05 10:26:40 -0500

    Add files via upload

---
+++ Assign3_func.py
@@ -0,0 +1,39 @@
+def o_piecesf():
+    orange = int(input("How many orange you want to buy?: "))
+    return orange
+
+def a_piecesf():
+    apple = int(input("How many apple you want to buy?: "))
+    return apple
+
+def priceOf():
+    priceO = 25
+    return priceO
+
+def priceAf():
+    priceA = 20
+    return priceA
+    
+def totalOf(orange, priceO):
+    total_orange = orange * priceO
+    return total_orange
+
+def totalAf(apple, priceA):
+    total_apple = apple * priceA
+    return total_apple
+
+def payf(total_apple, total_orange):
+    bill = total_apple + total_orange
+    return bill
+
+def displayoutput(bill):
+    print(f"The total amount you will pay is {bill}")
+
+orange = o_piecesf()
+apple = a_piecesf()
+priceO = priceOf()
+priceA = priceAf()
+total_orange = totalOf(orange, priceO)
+total_apple = totalAf(apple, priceA)
+bill = payf(total_apple, total_orange)
+displayoutput(bill)
